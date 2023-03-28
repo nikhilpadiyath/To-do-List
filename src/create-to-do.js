f9f644a528ca92ff8f071a00226a38b2288fc2be
nikhilpadiyath <nikhilpadiyath@gmail.com>
2023-03-28 20:38:46 +0530

    Added creating of to-do list

--- src/create-to-do.js
+++ src/create-to-do.js
@@ -1,7 +1,42 @@
+import { compareAsc, format, parseISO, startOfToday} from 'date-fns';
+import { clearForm } from './initial-dom-manip.js';
+
 let toDoArray = [];
-export const createToDo = (Title,Description,DueDate,Priority,Checklist) => {
-    console.log({Title,Description,DueDate,Priority,Checklist});
-    toDoArray.push({Title,Description,DueDate,Priority,Checklist});
+
+export const createToDo = () => {
+    let Title = document.getElementById("Title").value;
+    let Description = document.getElementById("Description").value;
+    let DueDate = document.getElementById("DueDate").value;
+    let Priority = document.getElementById("Priority").value;
+
+    if(Title=="" || Description=="" || DueDate==""){
+        alert("Title, Description and Due Date are required fields");
+        return;
+    }
+
+    if(parseISO(DueDate) < startOfToday()) {
+        alert("You have entered a previous date");
+        console.log("Due Date entered:",parseISO(DueDate));
+        console.log("Current date:",startOfToday());
+        return;
+    }
+
+    const nodeListCheckList = document.querySelectorAll("li");
+    let CheckListArray = [];
+    for (let i=0; i<nodeListCheckList.length; i++){
+        let strippedCheckList = nodeListCheckList[i].textContent.replace("\u00D7", '');
+        CheckListArray.push(strippedCheckList);
+    }
+
+    let CheckList = CheckListArray.join(", ");
+    console.log("Called CreateToDo module...creating todo now");
+    console.log({Title,Description,DueDate,Priority,CheckList});
+    console.log ("Pushing this object to the toDoArray");
+    toDoArray.push({Title,Description,DueDate,Priority,CheckList});
     console.log(toDoArray);
-    return {Title,Description,DueDate,Priority,Checklist};
+
+    clearForm();
+
+    return {Title,Description,DueDate,Priority,CheckList};
+
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+
 import { createToDo } from './create-to-do.js';
 import { blankProjectLoad } from './blank-project-load.js';
-import { initialdomManip } from './initial-dom-manip.js';
-
+import { displayDefaultProject, displayTheForm, addItemToChecklist, clearForm } from './initial-dom-manip.js';
+import './style.css';
+document.addEventListener('DOMContentLoaded',(event) => {
 blankProjectLoad();
-initialdomManip();
+//initialdomManip();
 
-const myToDo = createToDo("Grocery Run","Go get groceries","25/3/2023","Low", "Meat, Fish, Veggies");
-const myToDo2 = createToDo("Cricket","To play Cricket","26/3/2023","Medium","Bat, Ball, Stumps");
-console.log("Show me properties of myToDo",myToDo);
-console.log("Show me properties of myToDo2",myToDo2);
+let clickEventsModule = (function() {
+    const addNewToDo = document.querySelector(".add-todo-button");
+    addNewToDo.addEventListener("click", displayTheForm);
 
+    const addToChecklist = document.querySelector(".add-to-checklist");
+    addToChecklist.addEventListener("click", addItemToChecklist);
+
+    const clearButton = document.querySelector(".reset-btn");
+    clearButton.addEventListener("click",clearForm);
+
+    const submitButton = document.querySelector(".submit-btn");
+    submitButton.addEventListener("click", createToDo);
+
+})();
+
+//const myToDo = createToDo("Grocery Run","Go get groceries","25/3/2023","Low", "Meat, Fish, Veggies");
+//const myToDo2 = createToDo("Cricket","To play Cricket","26/3/2023","Medium","Bat, Ball, Stumps");
+//console.log("Show me properties of myToDo",myToDo);
+//console.log("Show me properties of myToDo2",myToDo2);
+})
